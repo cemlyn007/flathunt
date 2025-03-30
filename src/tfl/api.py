97bd6bcc1b00d1490588700950f56c920d50f037
Cemlyn <cemlynwaters@gmail.com>
2025-03-31 00:44:51 +0100

    feat: make tfl api more generic

--- src/tfl/api.py
+++ src/tfl/api.py
@@ -9,6 +9,8 @@ import urllib.parse
 import datetime
 import zoneinfo
 
+TIMEZONE = zoneinfo.ZoneInfo("Europe/London")
+
 
 class HTTPError(Exception): ...
 
@@ -20,78 +22,49 @@ class Mode(enum.StrEnum):
 
 
 class Journey(typing.NamedTuple):
-    duration_in_minutes: int
+    duration: datetime.timedelta
     departure_datetime: datetime.datetime
     arrival_datetime: datetime.datetime
     mode: Mode
     route_name: str
 
 
-class JourneyApi:
+class Tfl:
     def __init__(
         self,
         from_location: tuple[float, float],
         to_location: tuple[float, float],
         app_key: str,
-        arrival_time: datetime.time | None = None,
+        arrival_datetime: datetime.datetime | None = None,
     ) -> None:
         self._from_location = from_location
         self._to_location = to_location
         self._app_key = app_key
-        self._arrival_time = arrival_time
-        self._time_zone = zoneinfo.ZoneInfo("Europe/London")
+        self._arrival_datetime = arrival_datetime
 
-    def __call__(self) -> Journey | None:
-        if self._arrival_time is not None:
-            current_datetime = datetime.datetime.now(tz=self._time_zone)
-            if current_datetime.time() < datetime.time(hour=9):
-                arrival_datetime = current_datetime.replace(
-                    hour=self._arrival_time.hour,
-                    minute=self._arrival_time.minute,
-                    second=self._arrival_time.second,
-                    microsecond=self._arrival_time.microsecond,
-                )
-            else:
-                tomorrow = current_datetime.date() + datetime.timedelta(days=1)
-                arrival_datetime = datetime.datetime(
-                    tomorrow.year,
-                    tomorrow.month,
-                    tomorrow.day,
-                    hour=self._arrival_time.hour,
-                    minute=self._arrival_time.minute,
-                    second=self._arrival_time.second,
-                    microsecond=self._arrival_time.microsecond,
-                    tzinfo=self._time_zone,
-                )
-        else:
-            arrival_datetime = None
-        response = get_journey(
+    def __call__(self) -> list[Journey]:
+        response = get_journey_options(
             self._from_location,
             self._to_location,
-            arrival_datetime,
+            self._arrival_datetime,
             app_key=self._app_key,
         )
         raw_journeys = response["journeys"]
         journeys = [
             parse_journey(raw_journey, datetime.UTC) for raw_journey in raw_journeys
         ]
-        journeys = [
-            journey
-            for journey in journeys
-            if journey is not None and journey.mode == Mode.BUS
-        ]
+        arrival_datetime = self._arrival_datetime
         if arrival_datetime is None:
             journeys.sort(key=lambda journey: journey.arrival_datetime.timestamp())
         else:
             journeys.sort(
-                key=lambda journey: (
-                    arrival_datetime - journey.arrival_datetime
-                ).total_seconds()
+                key=lambda journey: arrival_datetime.timestamp()
+                - journey.arrival_datetime.timestamp()
             )
-        return journeys[0] if journeys else None
+        return journeys
 
 
-def get_journey(
+def get_journey_options(
     from_location: tuple[float, float],
     to_location: tuple[float, float],
     arrival_datetime: datetime.datetime | None,
@@ -113,29 +86,19 @@ def get_journey(
         parameters["date"] = date
         parameters["time"] = time
         parameters["timeIs"] = "departing"
-    else:
-        date = arrival_datetime.strftime("%Y%m%d")
-        time = arrival_datetime.strftime("%H%M")
-        parameters["date"] = date
-        parameters["time"] = time
-        parameters["timeIs"] = "arriving"
 
     connection = http.client.HTTPSConnection("api.tfl.gov.uk", port=443)
     try:
-        response = request(connection, "GET", url, parameters)
+        response = _request(connection, "GET", url, parameters)
     finally:
         connection.close()
     return response
 
 
-def parse_journey(
-    journey: dict[str, Any], time_zone: datetime.timezone
-) -> Journey | None:
+def parse_journey(journey: dict[str, Any], time_zone: datetime.timezone) -> Journey:
     duration_minutes = int(journey["duration"])
     departure_datetime = (
-        datetime.datetime.strptime(
-            journey["legs"][0]["departureTime"], "%Y-%m-%dT%H:%M:%S"
-        )
+        datetime.datetime.strptime(journey["startDateTime"], "%Y-%m-%dT%H:%M:%S")
         .replace(tzinfo=time_zone)
         .astimezone(time_zone)
     )
@@ -144,31 +107,48 @@ def parse_journey(
         .replace(tzinfo=time_zone)
         .astimezone(time_zone)
     )
-    if len(journey["legs"]) == 1 and journey["legs"][0]["mode"]["id"] == "walking":
-        return Journey(
-            duration_in_minutes=duration_minutes,
-            departure_datetime=departure_datetime,
-            arrival_datetime=arrival_datetime,
-            mode=Mode.WALKING,
-            route_name="",
-        )
-    elif len(journey["legs"]) != 3:
-        return None
-    elif journey["legs"][1]["mode"]["id"] != "bus":
-        raise ValueError("The middle leg should be a bus journey")
+    modes = [Mode(leg["mode"]["id"]) for leg in journey["legs"]]
+    if Mode.TUBE in modes:
+        mode = Mode.TUBE
+    elif Mode.BUS in modes:
+        mode = Mode.BUS
     else:
-        route_options = journey["legs"][1]["routeOptions"]
-        name = next(route_option["name"] for route_option in route_options)
-        return Journey(
-            duration_in_minutes=duration_minutes,
-            departure_datetime=departure_datetime,
-            arrival_datetime=arrival_datetime,
-            mode=Mode.BUS,
-            route_name=name,
-        )
+        mode = Mode.WALKING
+    route_names = []
+    for leg in journey["legs"]:
+        if "routeOptions" in leg:
+            name = next(option["name"] for option in leg["routeOptions"])
+            if name:
+                route_names.append(name)
+    route_name = "->".join(route_names)
+    return Journey(
+        duration=datetime.timedelta(minutes=duration_minutes),
+        departure_datetime=departure_datetime,
+        arrival_datetime=arrival_datetime,
+        mode=mode,
+        route_name=route_name,
+    )
 
 
-def request(
+def get_next_datetime(
+    arrival_time: datetime.time, timezone: datetime.tzinfo = TIMEZONE
+) -> datetime.datetime:
+    next_day = datetime.datetime.now(tz=timezone).date() + datetime.timedelta(days=1)
+    while next_day.weekday() > 4:
+        next_day = next_day + datetime.timedelta(days=1)
+    return datetime.datetime(
+        next_day.year,
+        next_day.month,
+        next_day.day,
+        hour=arrival_time.hour,
+        minute=arrival_time.minute,
+        second=arrival_time.second,
+        microsecond=arrival_time.microsecond,
+        tzinfo=timezone,
+    )
+
+
+def _request(
     connection: http.client.HTTPSConnection,
     method: str,
     url: str,
