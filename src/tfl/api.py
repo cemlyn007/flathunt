1e60933b7614056d51b0bc73d3e9c49305037bb9
Cemlyn Neil Waters <cemlynwaters@gmail.com>
2025-12-14 19:52:21 +0000

    feat: do not retry tfl api error

--- src/tfl/api.py
+++ src/tfl/api.py
@@ -249,6 +249,8 @@ def _is_retryable_error(exception: BaseException) -> bool:
         exception, (httpx.TimeoutException, httpx.NetworkError, httpx.ProtocolError)
     ):
         return True
+    elif isinstance(exception, exceptions.TflApiError):
+        return False
     elif not isinstance(exception, httpx.HTTPStatusError):
         return False
     status_code = exception.response.status_code
